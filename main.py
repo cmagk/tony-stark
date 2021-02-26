8116fa62de021b08613874de99cd0c309e386559
cmagk <30977494+cmagk@users.noreply.github.com>
2021-02-26 23:29:17 +0200

    Changed input values to consts.

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from UFunction import U
 from MFunction import get_m
 from QFunction import get_q, get_q_internal_1, get_q_internal_2, q_list
 import math
+NODE_COUNT_EXTERNAL = 30
+NODE_COUNT_ROOF = 30
+NODE_COUNT_INTERNAL = 30
+DELTA_T = 120  # Χρονικό βήμα sec
 # Απορροφητικότητα
 ABSORPTION = 0.8
 # Συντελεστής συναγωγής περιβάλλοντος
@@ -26,23 +30,16 @@ tin = 22
 RAD_LIST = [0, 0, 0, 0, 0, 0, 20, 40, 40, 50, 50, 100, 120, 250, 300, 200, 100, 50, 50, 40, 40, 0, 0, 0]
 WALLS_INDEX = {"NORTH": 0, "WEST": 1, "EAST": 2, "ROOF": 3, "INTERNAL": 4, "GLASSPANE": 5}
 
-
 def get_charge(q_wall, area):
     return q_wall * area
 
-
-node_count_external = int(input("Enter external node count:"))
-node_count_roof = int(input("Enter roof node count:"))
-node_count_internal = int(input("Enter internal node count:"))
-delta_t = int(input("Enter Delta T:"))  # Χρονικό βήμα sec
 node_count = {
-    "EXTERNAL": node_count_external,
-    "ROOF": node_count_roof,
-    "INTERNAL": node_count_internal
+    "EXTERNAL": NODE_COUNT_EXTERNAL,
+    "ROOF": NODE_COUNT_ROOF,
+    "INTERNAL": NODE_COUNT_INTERNAL
 }
 NORTH_WALL_THICKNESS = walls[WALLS_INDEX["NORTH"]].get_thickness()
 delta_x = NORTH_WALL_THICKNESS / node_count["EXTERNAL"]  # Δχ
-
 # Συντελεστές συναγωγής
 is_found = False
 loops_count = 1
@@ -68,8 +65,6 @@ u_pos_dict = {
             "INTERNAL": [i for i in range(2, node_count["INTERNAL"] + 1)]
         }
 
-print(t_out)
-
 while not is_found:
     counter = 0
     temps_close_counter = 0
@@ -84,7 +79,7 @@ while not is_found:
         u_list = U(WALLS_INDEX["NORTH"], node_count["EXTERNAL"], NORTH_WALL_THICKNESS, delta_x, H_OUT, H_IN,
                    u_pos_dict["EXTERNAL"]).get_u()
 
-        m_list = get_m(delta_x, delta_t, node_count["EXTERNAL"], NORTH_WALL_THICKNESS, WALLS_INDEX["NORTH"])
+        m_list = get_m(delta_x, DELTA_T, node_count["EXTERNAL"], NORTH_WALL_THICKNESS, WALLS_INDEX["NORTH"])
 
         (q_north_wall, new_T_north) = get_q(m_list, node_temperatures["EXTERNAL"]["NORTH"], u_list, current_tout,
                                             ABSORPTION, radiation, node_count["EXTERNAL"], temps_index_ext, tin)
@@ -112,7 +107,7 @@ while not is_found:
         roof_u_list = U(WALLS_INDEX["ROOF"], node_count["ROOF"], roof_wall_thickness, roof_delta_x, H_OUT, H_IN,
                         u_pos_dict["ROOF"]).get_u()
 
-        roof_m_list = m_list = get_m(roof_delta_x, delta_t, node_count["ROOF"], roof_wall_thickness,
+        roof_m_list = m_list = get_m(roof_delta_x, DELTA_T, node_count["ROOF"], roof_wall_thickness,
                                      WALLS_INDEX["ROOF"])
 
         (q_roof_wall, new_T_roof) = get_q(roof_m_list, node_temperatures["ROOF"], roof_u_list, current_tout, ABSORPTION,
@@ -128,7 +123,7 @@ while not is_found:
         internal_u_list = U(WALLS_INDEX["INTERNAL"], node_count["INTERNAL"], internal_wall_thickness, internal_delta_x,
                             H_OUT, H_IN, u_pos_dict["INTERNAL"]).get_u()
 
-        internal_m_list = get_m(internal_delta_x, delta_t, node_count["INTERNAL"], internal_wall_thickness,
+        internal_m_list = get_m(internal_delta_x, DELTA_T, node_count["INTERNAL"], internal_wall_thickness,
                                 WALLS_INDEX["INTERNAL"])
 
         (q_internal_wall1, new_T_internal) = get_q_internal_1(internal_m_list, node_temperatures["INTERNAL"],
@@ -155,8 +150,8 @@ while not is_found:
         mc_area = f * mc_air
         sq += q_infilt
 
-        tin = tin + (sq * delta_t) / mc_area
-        counter += delta_t
+        tin = tin + (sq * DELTA_T) / mc_area
+        counter += DELTA_T
         daily_tins.append(tin)
 
     tins.append(daily_tins)
